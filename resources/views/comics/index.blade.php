900bd146227dce329fa957b549af21783a6932ce
FraAccaBi <francescahuda@gmail.com>
2022-06-16 15:49:13 +0200

    add index route commit

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+@section('content')
+<table class="table">
+    <thead>
+        <tr>
+            <th>id</th>
+            <th>title</th>
+            <th>description</th>
+            <th>thumb</th>
+            <th>price</th>
+            <th>series</th>
+            <th>sale_date</th>
+            <th>type</th>
+
+            <th>actions</th>
+        </tr>
+    </thead>
+    <tbody>
+
+    @foreach($comics as $comic)
+        <tr>
+            <td scope="row">{{$comic->id}}</td>
+            <th>title</th>
+            <td>{{$comic->title}}</td>
+            <th>{{$comic->description}}</th>
+            <td><img width="60px" height="60px" src="{{$comic->thumb}}" alt=""> </td>
+            <td>{{$comic->price}}</td>
+            <td>{{$comic->series}}</td>
+            <td>{{$comic->sale_date}}</td>
+            <td>{{$comic->type}}</td>
+
+            <td>View - Edit -Delete</td>
+        </tr>
+
+        @endforeach
+    </tbody>
+</table>
+
+
+@endsection
